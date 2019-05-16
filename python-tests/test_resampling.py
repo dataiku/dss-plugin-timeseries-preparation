f2f03459426575ddf3eb1bbba87002a76e2770d1
du-phan <duphan@Du-MacBook.local>
2019-05-16 15:55:54 +0200

    add tests for groupby_cols functionalities in resampling

--- python-tests/test_resampling.py
+++ python-tests/test_resampling.py
@@ -7,6 +7,7 @@ import sys
 import os
 import random
 from datetime import datetime
+import numpy as np
 
 ## Add stuff to the path to enable exec outside of DSS
 plugin_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -21,6 +22,7 @@ JUST_BEFORE_FALL_DST = pd.Timestamp('20191027 02:59:00').tz_localize('CET', ambi
 
 TIME_COL = 'time_col'
 DATA_COL = 'data_col'
+GROUP_COL = 'group_col'
 
 ### Helpers to create test data, should be fixtures at some point I guess
 def _make_df_with_one_col(column_data, period=pd.DateOffset(seconds=1), start_time=JUST_BEFORE_SPRING_DST):
@@ -31,14 +33,25 @@ def _make_df_with_one_col(column_data, period=pd.DateOffset(seconds=1), start_ti
     df = pd.DataFrame({TIME_COL: time, DATA_COL: column_data})
     return df
 
+def _make_df_with_one_col_group(column_data, num_group, period=pd.DateOffset(seconds=1), start_time=JUST_BEFORE_SPRING_DST):
+    
+    df_list = []
+    for x in xrange(num_group):
+        group_name = 'group_{}'.format(x)
+        temp_df = _make_df_with_one_col(column_data, period=period)
+        temp_df[GROUP_COL] = group_name
+        df_list.append(temp_df)
+    df = pd.concat(df_list, axis=0)
+    return df
 
-def _make_resampling_params():
-    params = dku_timeseries.ResamplerParams()
+
+def _make_resampling_params(groupby_cols=None):
+    params = dku_timeseries.ResamplerParams(groupby_cols=groupby_cols)
     params.datetime_column = TIME_COL
     return params
 
-def _make_resampler():
-    params = _make_resampling_params()
+def _make_resampler(groupby_cols=None):
+    params = _make_resampling_params(groupby_cols)
     return dku_timeseries.Resampler(params)
 
 
@@ -84,3 +97,49 @@ def test_half_freq_resampling():
     assert output_df.shape[0]-1 == half_length
     for x in range(100):
         assert output_df[DATA_COL][x] == 2*x
+
+def test_identity_resampling_group():
+    """
+    Default sampling rate is 1Hz
+    Since we create test data at 1Hz, default resampling should be identity
+    """
+    length = 100000
+    num_group = 3
+    print("test_identity_resampling with "+str(length)+" records")
+    data = [random.random() for _ in range(length)]
+    df = _make_df_with_one_col_group(data, num_group=num_group)
+    resampler = _make_resampler(groupby_cols=GROUP_COL)
+    df2 = resampler.transform(df, TIME_COL)
+    assert len(df2) == length*num_group
+        
+    for x in xrange(num_group):
+        print(x)
+        df_ref = df.loc[df[GROUP_COL] == 'group_{}'.format(x), [TIME_COL, DATA_COL]].reset_index()
+        df_ref = df_ref.sort_values(TIME_COL)
+
+        df_check = df2.loc[df2[GROUP_COL] == 'group_{}'.format(x), [TIME_COL, DATA_COL]].reset_index()
+        df_check = df_check.sort_values(TIME_COL)
+
+        for y in xrange(1000):
+            assert df_check[DATA_COL][y] == df_ref[DATA_COL][y]
+
+def test_half_freq_resampling_group():
+    length = 100000
+    num_group = 3
+    print("test_identity_resampling_group with "+str(2*length)+" records")
+    data = [x for x in np.arange(0, length, 0.5)]
+    df = _make_df_with_one_col_group(data, num_group=num_group, period=pd.DateOffset(seconds=0.5))
+    resampler = _make_resampler(GROUP_COL)
+    df2 = resampler.transform(df, TIME_COL)
+
+    for x in xrange(num_group):
+        print(x)
+        df_ref = df.loc[df[GROUP_COL] == 'group_{}'.format(x), [TIME_COL, DATA_COL]].reset_index()
+        df_ref = df_ref.sort_values(TIME_COL)
+
+        df_check = df2.loc[df2[GROUP_COL] == 'group_{}'.format(x), [TIME_COL, DATA_COL]].reset_index()
+        df_check = df_check.sort_values(TIME_COL)
+
+        for y in xrange(1000):
+            #print y, df_check[DATA_COL][y], df_ref[DATA_COL][y]
+            assert df_check[DATA_COL][y] == 2*df_ref[DATA_COL][y]
