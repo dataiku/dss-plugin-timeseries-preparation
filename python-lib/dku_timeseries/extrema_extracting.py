aab76e8a0dae8a3a4cd6af5bbae1ec35f210f7f8
Du Phan <dphan@MacBook-Pro-de-Dataiku-3.local>
2019-05-21 10:12:13 +0200

    update extrema_extractor

--- python-lib/dku_timeseries/extrema_extracting.py
+++ python-lib/dku_timeseries/extrema_extracting.py
@@ -7,12 +7,12 @@ from pandas.tseries.frequencies import to_offset
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='timeseries-preparation plugin %(levelname)s - %(message)s')
 
-
 EXTREMA_TYPES = ['min', 'max']
+
 
 class ExtremaExtractorParams:
 
-    def __init__(self, window_roller = None, extrema_type = 'max'):
+    def __init__(self, window_roller=None, extrema_type='max'):
         self.window_roller = window_roller
         self.extrema_type = extrema_type
 
@@ -20,7 +20,8 @@ class ExtremaExtractorParams:
         if self.window_roller is None:
             raise ValueError('WindowRoller object is not specified.')
         if self.extrema_type not in EXTREMA_TYPES:
-            raise ValueError('{0} is not a valid options. Possible extrema types are: {1}'.format(self.extrema_type, EXTREMA_TYPES))
+            raise ValueError(
+                '{0} is not a valid options. Possible extrema types are: {1}'.format(self.extrema_type, EXTREMA_TYPES))
 
 
 class ExtremaExtractor:
@@ -38,26 +39,28 @@ class ExtremaExtractor:
         aggregated statistics on what's going on around the extrema.
         """
         df = raw_df.set_index(datetime_column).sort_index()
-        df = df.fillna(0)
+        # df = df.fillna(0)
         if groupby_columns:
             grouped = df.groupby(groupby_columns)
             computed_groups = []
             for group_id, group in grouped:
-                try: #TODO remove the try except (it was to avoid empty, nan dataset)
-                    extrema_neighbor_df, extrema_value = self._find_extrema_neighbor_zone(group, extrema_column)
-                    rolling_df = self.params.window_roller.compute(extrema_neighbor_df)
-                    extrema_df = rolling_df.loc[group[extrema_column]==extrema_value]
-                    extrema_df = extrema_df.rename_axis(datetime_column).reset_index()
-                    computed_groups.append(extrema_df)
-                except Exception as e:
-                    logging.warning('{}:{}'.format(group_id, e.message))
-                    continue
+                print group_id
+                extrema_neighbor_df, extrema_value = self._find_extrema_neighbor_zone(group, extrema_column)
+                if extrema_neighbor_df is None:
+                    extrema_df = pd.DataFrame({groupby_columns[0]: [group_id]})
+                else:
+                    extrema_neighbor_df = extrema_neighbor_df.rename_axis(datetime_column).reset_index()
+                    rolling_df = self.params.window_roller.compute(extrema_neighbor_df, datetime_column)
+                    extrema_df = rolling_df.loc[rolling_df[extrema_column] == extrema_value]
+                    extrema_df[groupby_columns[0]] = group_id
+
+                computed_groups.append(extrema_df)
             final_df = pd.concat(computed_groups)
         else:
             extrema_neighbor_df, extrema_value = self._find_extrema_neighbor_zone(df, extrema_column)
-            rolling_df = self.params.window_roller.compute(extrema_neighbor_df)
-            final_df = rolling_df.loc[df[extrema_column]==extrema_value]
-            final_df = final_df.rename_axis(datetime_column).reset_index()
+            extrema_neighbor_df = extrema_neighbor_df.rename_axis(datetime_column).reset_index()
+            rolling_df = self.params.window_roller.compute(extrema_neighbor_df, datetime_column)
+            final_df = rolling_df.loc[rolling_df[extrema_column] == extrema_value].reset_index()
         return final_df
 
     def _find_extrema_neighbor_zone(self, df, extrema_column):
@@ -72,6 +75,8 @@ class ExtremaExtractor:
             df_neighbor = df.loc[start_time:end_time]
             extrema_neigbors.append(df_neighbor)
 
-        extrema_neighbor_df = pd.concat(extrema_neigbors).drop_duplicates()
-
-        return extrema_neighbor_df, extrema_value
+        if len(extrema_neigbors) > 0:
+            extrema_neighbor_df = pd.concat(extrema_neigbors).drop_duplicates()
+            return extrema_neighbor_df, extrema_value
+        else:
+            return None, None
