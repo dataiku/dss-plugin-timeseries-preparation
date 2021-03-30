cef8c21585f0cc0bacb4b380ccaebd312ff6d374
Marsobad <marine.sobas@dataiku.com>
2021-03-30 19:13:39 +0200

    fix UI transition for ts identifiers [ch62730]

--- tests/python/unit/dku_timeseries/test_recipe_config_loading.py
+++ tests/python/unit/dku_timeseries/test_recipe_config_loading.py
@@ -12,18 +12,29 @@ def config():
     return config
 
 
+@pytest.fixture
+def dataset_columns():
+    return ["value1", "country", "old"]
+
+
 class TestRecipeConfigLoading:
     def test_check_and_get_groupby_columns(self, config):
         dataset_columns = ["value1", "country", "old"]
         groupby_colums = check_and_get_groupby_columns(config, dataset_columns)
         assert groupby_colums == ["country", "old"]
 
+        config.pop("groupby_columns")
+        config["groupby_column"] = "country"
+        groupby_colums = check_and_get_groupby_columns(config, dataset_columns)
+        assert groupby_colums == ["country"]
+
+    def test_invalid_groupby_columns(self, config, dataset_columns):
+        config["groupby_columns"] = ["country", ""]
         restricted_dataset_columns = ["value1", "country"]
         with pytest.raises(ValueError) as err:
             _ = check_and_get_groupby_columns(config, restricted_dataset_columns)
         assert "Invalid time series identifiers selection" in str(err.value)
 
-        config["groupby_columns"] = ["country", ""]
         with pytest.raises(ValueError) as err:
             _ = check_and_get_groupby_columns(config, restricted_dataset_columns)
         assert "Invalid time series identifiers selection" in str(err.value)
@@ -32,22 +43,14 @@ class TestRecipeConfigLoading:
         with pytest.raises(ValueError) as err:
             _ = check_and_get_groupby_columns(config, restricted_dataset_columns)
         assert "Long format is activated but no time series identifiers have been provided" in str(err.value)
-
-        config["groupby_column"] = "country"
-        groupby_colums = check_and_get_groupby_columns(config, dataset_columns)
-        assert groupby_colums == ["country"]
-
-        config["groupby_columns"] = ["notice"]
-        groupby_colums = check_and_get_groupby_columns(config, dataset_columns)
-        assert groupby_colums == ["country"]
 
         config["groupby_column"] = "not_ok"
         with pytest.raises(ValueError) as err:
             _ = check_and_get_groupby_columns(config, dataset_columns)
         assert "Invalid time series identifiers selection" in str(err.value)
 
-        config.pop("groupby_column")
-        config.pop("groupby_columns")
-        with pytest.raises(ValueError) as err:
-            _ = check_and_get_groupby_columns(config, dataset_columns)
-        assert "Long format is activated but no time series identifiers have been provided" in str(err.value)
+    def test_two_identifiers_fields(self, config, dataset_columns):
+        config["groupby_column"] = "notice"
+        config["groupby_columns"] = ["country"]
+        groupby_colums = check_and_get_groupby_columns(config, dataset_columns)
+        assert groupby_colums == ["country"]
