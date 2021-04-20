3126d4cfa2202c8d16cf43e4973a8521113d490d
Marsobad <marine.sobas@dataiku.com>
2021-04-20 12:30:22 +0200

    add additional parameters to a single file

--- python-lib/dku_config/additional_parameter.py
+++ python-lib/dku_config/additional_parameter.py
@@ -1,3 +1,6 @@
+from dku_config.utils import is_positive_int, is_odd
+
+
 class AdditionalParameter:
     def __init__(self, name, value):
         self.name = name
@@ -17,3 +20,31 @@ class AdditionalParameter:
             name=self.name,
             error=self.error_message
         )
+
+
+class AdditionalDegree(AdditionalParameter):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+        self.error_message += "It must be equal to 0 or 1."
+
+    def is_valid(self, dku_config):
+        return self.value in [0, 1]
+
+
+class AdditionalSmoother(AdditionalParameter):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+        self.error_message += "It must be an odd positive integer greater than 3 and the season length."
+
+    def is_valid(self, dku_config):
+        minimum = max(dku_config.season_length, 3)
+        return (self.value is None) or (is_odd(self.value) and self.value > minimum)
+
+
+class AdditionalSpeedup(AdditionalParameter):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+        self.error_message += "It must be a positive integer."
+
+    def is_valid(self, dku_config):
+        return is_positive_int(self.value)
