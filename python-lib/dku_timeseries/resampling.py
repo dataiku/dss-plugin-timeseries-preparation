077ac64f4419d9b998f4b60dfc10d7a0b9f0e8d8
admin <admin@dataiku.com>
2025-05-01 12:21:11 +0200

    Edited file 'python-lib/dku_timeseries/resampling.py'

--- python-lib/dku_timeseries/resampling.py
+++ python-lib/dku_timeseries/resampling.py
@@ -240,8 +240,6 @@ class Resampler:
             for col in category_columns:
                 # only perform conversion if the column has a timezone
                 if pd.api.types.is_datetime64_any_dtype(category_filled_df[col]) and category_filled_df[col].dt.tz is not None:
-                    import pydevd_pycharm
-                    pydevd_pycharm.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
                     if most_frequent_categoricals[col].tzinfo is None: # tz-naive timestamp -> localize
                         most_frequent_categoricals[col] = most_frequent_categoricals[col].tz_localize("UTC")
                     else: # tz_convert
